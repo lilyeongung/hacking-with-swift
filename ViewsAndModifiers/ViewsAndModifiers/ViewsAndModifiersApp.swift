0139c2047d6c14fd55b48fcb9f763399f0a9d482
lil yeongung <148127804+lilyeongung@users.noreply.github.com>
2024-05-17 10:30:07 -0700

    update

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Andrew Park on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
